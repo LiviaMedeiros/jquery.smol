c7f8a135965dada997bdc9ba9060d6b413922f78
LiviaMedeiros <livia@cirno.name>
2020-05-18 18:41:32 +0200

    11611967adf2bd9ff4304132f917629ec1134049

--- dist/jquery.smol.js
+++ dist/jquery.smol.js
@@ -2,7 +2,7 @@
  * jQuery JavaScript Library v4.0.0-pre -core/ready,-ajax/jsonp,-ajax/load,-ajax/script,-ajax/var/location,-ajax/var/nonce,-ajax/var/rquery,-ajax/xhr,-attributes/val,-attributes/prop,-css/hiddenVisibleSelectors,-css/showHide,-effects,-effects/Tween,-effects/animatedSelector,-deferred/exceptionHook,-deprecated/event,-deprecated/ajax-event-alias,-queue/delay,-exports/amd,-manipulation/_evalUrl,-ajax,-callbacks,-deferred,-queue,-deprecated,-dimensions,-offset,-serialize,-wrap
  * https://jquery.com/
  *
- * Copyright JS Foundation and other contributors
+ * Copyright OpenJS Foundation and other contributors
  * Released under the MIT license
  * https://jquery.org/license
  *
