b563d0e4fa736c11c55c59c4f8d4bdd384cceefe
LiviaMedeiros <livia@cirno.name>
2021-01-11 11:56:08 -0500

    3bbbc11111840d6fd5160db13f2c1a9acb05c4c4

--- dist/jquery.smol.js
+++ dist/jquery.smol.js
@@ -5195,6 +5195,53 @@ function finalPropName( name ) {
 	return vendorProps[ name ] = vendorPropName( name ) || name;
 }
 
+( function() {
+
+var reliableTrDimensionsVal,
+	div = document.createElement( "div" );
+
+// Finish early in limited (non-browser) environments
+if ( !div.style ) {
+	return;
+}
+
+// Support: IE 10 - 11+
+// IE misreports `getComputedStyle` of table rows with width/height
+// set in CSS while `offset*` properties report correct values.
+// Support: Firefox 70+
+// Only Firefox includes border widths
+// in computed dimensions. (gh-4529)
+support.reliableTrDimensions = function() {
+	var table, tr, trStyle;
+	if ( reliableTrDimensionsVal == null ) {
+		table = document.createElement( "table" );
+		tr = document.createElement( "tr" );
+
+		table.style.cssText = "position:absolute;left:-11111px;border-collapse:separate";
+		tr.style.cssText = "border:1px solid";
+
+		// Support: Chrome 86+
+		// Height set through cssText does not get applied.
+		// Computed height then comes back as 0.
+		tr.style.height = "1px";
+		div.style.height = "9px";
+
+		documentElement
+			.appendChild( table )
+			.appendChild( tr )
+			.appendChild( div );
+
+		trStyle = window.getComputedStyle( tr );
+		reliableTrDimensionsVal = ( parseInt( trStyle.height, 10 ) +
+				parseInt( trStyle.borderTopWidth, 10 ) +
+				parseInt( trStyle.borderBottomWidth, 10 ) ) === tr.offsetHeight;
+
+		documentElement.removeChild( table );
+	}
+	return reliableTrDimensionsVal;
+};
+} )();
+
 var
 
 	// Swappable if display is none or starts with table
@@ -5312,23 +5359,24 @@ function getWidthOrHeight( elem, dimension, extra ) {
 	}
 
 
-	if ( ( isIE &&
-		(
-
-			// Support: IE 9 - 11+
-			// Use offsetWidth/offsetHeight for when box sizing is unreliable.
-			// In those cases, the computed value can be trusted to be border-box.
-			isBorderBox ||
-
-			// Support: IE 10 - 11+
-			// IE misreports `getComputedStyle` of table rows with width/height
-			// set in CSS while `offset*` properties report correct values.
-			nodeName( elem, "tr" )
-		) ||
+	if ( (
 
 		// Fall back to offsetWidth/offsetHeight when value is "auto"
 		// This happens for inline elements with no explicit setting (gh-3571)
-		val === "auto" ) &&
+		val === "auto" ||
+
+		// Support: IE 9 - 11+
+		// Use offsetWidth/offsetHeight for when box sizing is unreliable.
+		// In those cases, the computed value can be trusted to be border-box.
+		( isIE && isBorderBox ) ||
+
+		// Support: IE 10 - 11+
+		// IE misreports `getComputedStyle` of table rows with width/height
+		// set in CSS while `offset*` properties report correct values.
+		// Support: Firefox 70+
+		// Firefox includes border widths
+		// in computed dimensions for table rows. (gh-4529)
+		( !support.reliableTrDimensions() && nodeName( elem, "tr" ) ) ) &&
 
 		// Make sure the element is visible & connected
 		elem.getClientRects().length ) {
